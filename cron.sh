76970e089a0342ccc7ed72fe712a8bbc0d89bc47
Eric Dill <edill@bnl.gov>
2016-05-06 13:24:47 -0400

    Download the source for the conda builds into the dockerfile
    Make /tmp writable by everyone
    
    Clone recipes into builder's home directory
    
    Update build script
    
    Maybe fix the cron script
    
    foo

--- cron.sh
+++ cron.sh
@@ -6,12 +6,23 @@ echo "
 docker pull $BASE_IMAGE
 GIT_REPO='.'
 
+DOCKERFILE_FOLDER=$GIT_REPO/nsls2-conda-builder
+
+echo "
+    Download the source repos
+"
+CLONE_BASE=/tmp/$LOGNAME/debian-conda-builder
+rm -rf $CLONE_BASE
+mkdir -p $CLONE_BASE
+cd $CLONE_BASE
+git clone https://github.com/nsls-ii/staged-recipes-dev
+git clone https://github.com/nsls-ii/auto-build-tagged-recipes
+git clone https://github.com/nsls-ii/skbeam-recipes
+
 echo "
     Copying nsls2 certificate to be a sibling of the nsls2 conda builder
     dockerfile
 "
-DOCKERFILE_FOLDER=$GIT_REPO/nsls2-conda-builder
-
 cp ~/certificates/ca_cs_nsls2_local.crt $DOCKERFILE_FOLDER
 cp ~/dev/dotfiles/tokens/edill.anaconda.nsls2.token $DOCKERFILE_FOLDER
 NSLS2_IMAGE="ericdill/nsls2-conda-builder"
@@ -19,17 +30,21 @@ NSLS2_IMAGE="ericdill/nsls2-conda-builder"
 echo "
     Building nsls2-conda-builder docker image named $NSLS2_IMAGE
 "
-docker build -t $NSLS2_IMAGE $DOCKERFILE_FOLDER 
+docker build -t $NSLS2_IMAGE $DOCKERFILE_FOLDER
 
 
 DEV_CHANNEL=nsls2-dev-testing
 TAG_CHANNEL=nsls2-tag-testing
 echo "
-    Running docker image named $NSLS2_IMAGE with the following environmental 
+    Running docker image named $NSLS2_IMAGE with the following environmental
     variables:
 
     BINSTAR_TOKEN=$BINSTAR_TOKEN
     DEV_CHANNEL=$DEV_CHANNEL
     TAG_CHANNEL=$TAG_CHANNEL
 "
-docker run -e BINSTAR_TOKEN=$BINSTAR_TOKEN -e DEV_CHANNEL=nsls2-dev-testing -e TAG_CHANNEL=nsls2-tag-testing $NSLS2_IMAGE:latest
+docker run \
+    -e BINSTAR_TOKEN=$BINSTAR_TOKEN \
+    -e DEV_CHANNEL=nsls2-dev-testing \
+    -e TAG_CHANNEL=nsls2-tag-testing \
+    $NSLS2_IMAGE:latest
--- nsls2-conda-builder/build.sh
+++ nsls2-conda-builder/build.sh
@@ -13,23 +13,22 @@ cd
 export CONDA_NPY=1.10
 
 echo "
-    Building tagged recipes
+
+Building tagged recipes
+
 "
 conda config --add channels $TAG_CHANNEL --force
 YAML_FILE='~/auto-build-tagged-recipes/build-directive.yaml'
 # replace the scikit-beam url with the local copy we downloaded
 # in the docker file
 sed -i 's/https:\/\/github.com\/scikit-beam\/skbeam-recipes/~\/skbeam-recipes/g' $YAML_FILE
-# show the contents
-echo "
-    Contents of build directive:
-"
-cat $YAML_FILE
 # actually build it
 build_from_yaml $YAML_FILE -u $TAG_CHANNEL
 
 echo "
-    Building dev recipes
+
+Building dev recipes
+
 "
 conda config --add channels $DEV_CHANNEL --force
 build_from_yaml ~/staged-recipes-dev/build-directive.yaml -u $TAG_CHANNEL
